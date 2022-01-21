7dd15959bdc6cad2512b4ed4aefb34d4b54533e3
bmcdonough <bmcdonough@netscout.com>
2022-01-21 15:17:09 -0500

    arguement parser

--- imap-search.py
+++ imap-search.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
+import argparse
+import sys
+
 def main():
+    parser = argparse.ArgumentParser(
+        description='login to IMAP and search for email')
+    parser.add_argument(
+        '--host', type=str, help='IMAP hostname: imap.domain.com', metavar='')
+    parser.add_argument(
+        '--user', type=str, help='Username', metavar='')
+    parser.add_argument(
+        '--pass', type=str, help='Password', metavar='')
+    parser.add_argument(
+        '--debug', help='Debug this script', action='store_true')
+
+    global args
+    args = parser.parse_args()
+
     return 0
 
 if __name__ == '__main__':
